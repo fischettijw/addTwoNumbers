434c4254632c2ade570b2f57ea9b141cdc5bcc56
Joseph Fischetti <fischettijw@gmail.com>
2019-08-25 15:23:56 -0400

    1 st commit

--- frmAddTwoNumbers.vb
+++ frmAddTwoNumbers.vb
@@ -2,4 +2,8 @@
     Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         btnAdd.Text = CInt(txtFirst.Text) + CInt(txtSecond.Text)
     End Sub
+
+    Private Sub FrmAddTwoNumbers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim a As Integer
+    End Sub
 End Class
